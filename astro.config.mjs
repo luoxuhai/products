ed593590c5628fc348d1308d52dba9f95f0aeea3
Darkce <15186743693@163.com>
2026-06-08 19:47:36 +0800

    Set English as default locale

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,12 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://example.com',
+  i18n: {
+    locales: ['en', 'zh-cn', 'zh-tw', 'ja'],
+    defaultLocale: 'en',
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
+  },
 });
