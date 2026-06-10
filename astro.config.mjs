edaf37cf6aa7b4227c04d11a26d088746fc4c5e4
Darkce <darkce97@gmail.com>
2026-06-10 15:02:41 +0000

    feat: add icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://luoxuhai.com',
   i18n: {
     locales: ['en', 'zh-cn', 'zh-tw', 'ja'],
     defaultLocale: 'en',
